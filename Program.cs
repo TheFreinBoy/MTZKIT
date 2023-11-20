4ecb464a777388465e64195baee2d723e3ff9012
TheFreinBoy <147555172+TheFreinBoy@users.noreply.github.com>
2023-11-20 20:47:31 +0200

    Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World23!");
         }
      }
 }
